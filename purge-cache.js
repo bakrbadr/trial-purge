61fc8fb5f117713a093e19a311549e46b2c77722
bakrbadr <50487900+bakrbadr@users.noreply.github.com>
2026-05-20 10:43:38 +0200

    Refactor purge-cache.js for variable declaration and auth

--- purge-cache.js
+++ purge-cache.js
@@ -1,19 +1,30 @@
-const ZONE_ID = "e29fc7fde8ab110462fd22c8be847284";
-const HOSTNAMES = ["bakrbadr.xyz"];
+var ZONE_ID = "e29fc7fde8ab110462fd22c8be847284";
+var HOSTNAMES = ["bakrbadr.xyz"];
 
-export default {
+var purge_cache_default = {
   async scheduled(event, env, ctx) {
     console.log("Cron triggered at:", event.scheduledTime);
+    
+    // Read the variables from the 'env' parameter
+    const authEmail = env.CF_AUTH_EMAIL;
+    const authKey = env.CF_AUTH_KEY;
 
-    // Send the purge request
     await fetch(`https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/purge_cache`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'X-Auth-Email': 'bakrbadr1@gmail.com',
-        'X-Auth-Key': '098e2907a36193bd5af6d1872d8672c7f4b34',
-        'Content-Type': 'application/json',
+        "X-Auth-Email": authEmail,
+        "X-Auth-Key": authKey,
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify({ hosts: HOSTNAMES }),
+      body: JSON.stringify({ hosts: HOSTNAMES })
     });
   },
-}
+  
+  async fetch(request, env, ctx) {
+    return new Response("Cron worker active.", { status: 200 });
+  }
+};
+
+export {
+  purge_cache_default as default
+};
